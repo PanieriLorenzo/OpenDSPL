7ee303b28820789f9b665be4d9282032e09ebebb
Lorenzo Panieri <panierilorenzo@gmail.com>
2020-11-19 17:01:05 +0100

    making interpreter

--- src/dspl_interpret.py
+++ src/dspl_interpret.py
@@ -8,3 +8,19 @@ import dspl_types as tp
 
 def interpret(program: tp.Block):
     pass
+
+# === MERGE ALL LAMBDAS WITH CALLER ============================================
+def _flattenBlock(blk: tp.Block):
+    pass
+
+# === COMPACT RECORD ACCESSES ==================================================
+def _compressBlock(blk: tp.Block):
+    pass
+
+# === EXTRACT SOURCES ==========================================================
+def _extractSrc(blk: tp.Block):
+    pass
+
+# === EXTRACT SINKS ============================================================
+def _extractSnk(blk: tp.Block):
+    pass
